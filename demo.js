0f6f3e3fafe97a0d59642bc3b92ebfecc608e046
Prince verma <prince.verma@dgliger.com>
2022-05-08 18:32:52 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
 const open = window.XMLHttpRequest.prototype.open;
-
+console.log("inside the function-------");
 function openReplacement() {
+  console.log("inside the function");
   this.addEventListener("load", function () {
     if (
       [
@@ -15,9 +16,9 @@ function openReplacement() {
   });
   return open.apply(this, arguments);
 }
-
+console.log("inside the function((((((((");
 window.XMLHttpRequest.prototype.open = openReplacement;
-
+console.log("inside the function&&&&&");
 function calculateShipping(cartJson) {
   console.log("calculate new shipping");
   console.log(JSON.parse(cartJson))
